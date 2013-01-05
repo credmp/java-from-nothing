dd2eac0cce8b3bd3499d1bab86a0dc7ed6462ec8
Arjen Wiersma <arjen@wiersma.org>
2013-01-06 00:24:11 +0100

    Basic pom.xml for a working application

---
+++ src/main/java/org/credmp/fromnothing/model/Actor.java
@@ -0,0 +1,13 @@
+package org.credmp.apps.fromnothing.model;
+
+public class Actor {
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
